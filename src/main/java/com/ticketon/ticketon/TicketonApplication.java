6dbddebb5befc1faaa809120e1a65e0f714ce211
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-05-26 02:01:30 +0900

    feat : Commit Init

---
+++ src/main/java/com/ticketon/ticketon/TicketonApplication.java
@@ -0,0 +1,13 @@
+package com.ticketon.ticketon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketonApplication.class, args);
+	}
+
+}
