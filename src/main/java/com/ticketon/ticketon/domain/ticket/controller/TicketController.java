6ea36fba036d981af7d806dcdadea2febee31ba6
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-05-26 17:53:05 +0900

    feat : Loki + Promtail 을 이용한 로그 수집 환경 구축

--- src/main/java/com/ticketon/ticketon/domain/ticket/controller/TicketController.java
+++ src/main/java/com/ticketon/ticketon/domain/ticket/controller/TicketController.java
@@ -1,4 +1,25 @@
 package com.ticketon.ticketon.domain.ticket.controller;
 
+import com.ticketon.ticketon.domain.ticket.service.TicketService;
+import com.ticketon.ticketon.utils.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
 public class TicketController {
+
+    private final TicketService ticketService;
+
+    @GetMapping("/tickets")
+    public ResponseEntity<?> testapi(){
+        log.info("TEST LOG");
+        SuccessResponse<?> response = new SuccessResponse<>(true,"test", null);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
