cca8e3c423d63dc8054ac1037849efff937cda24
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-05-27 04:34:28 +0900

    feat : Otel Collector + Otel 메트릭 수집 환경 설정

--- src/main/java/com/ticketon/ticketon/domain/ticket/service/TicketService.java
+++ src/main/java/com/ticketon/ticketon/domain/ticket/service/TicketService.java
@@ -1,7 +1,16 @@
 package com.ticketon.ticketon.domain.ticket.service;
 
+import com.ticketon.ticketon.domain.ticket.repository.TicketRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    public void save(){
+
+    }
 }
