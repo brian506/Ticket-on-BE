decc86d3c3eef981a5896f1047dce9d2a200f3fc
onagu7167 <onagu7167@gmail.com>
2025-06-07 19:05:17 +0900

    feat: 기본 회원가입/로그인/로그아웃

--- src/main/java/com/ticketon/ticketon/domain/member/service/MemberService.java
+++ src/main/java/com/ticketon/ticketon/domain/member/service/MemberService.java
@@ -1,7 +1,7 @@
 package com.ticketon.ticketon.domain.member.service;
 
 import com.ticketon.ticketon.domain.member.entity.Member;
-import com.ticketon.ticketon.domain.member.entity.dto.AddMemberRequest;
+import com.ticketon.ticketon.domain.member.entity.dto.MemberSingupRequest;
 import com.ticketon.ticketon.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +14,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public long save(AddMemberRequest dto) {
+    public long save(MemberSingupRequest dto) {
         return memberRepository.save(Member.builder()
                         .email(dto.getEmail())
                         .password(bCryptPasswordEncoder.encode(dto.getPassword()))
