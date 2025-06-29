e19c066dc89178d8258ed2ff5d77eb1165d2e67e
onagu7167@gmail.com <72810708+yeongsol-Kim@users.noreply.github.com>
2025-06-29 12:09:34 +0900

    refactor: 웹소켓 대기열 순번 조회시 사용자 검증 로직 추가

--- src/main/java/com/ticketon/ticketon/domain/waiting_queue/controller/QueueWebSocketController.java
+++ src/main/java/com/ticketon/ticketon/domain/waiting_queue/controller/QueueWebSocketController.java
@@ -1,6 +1,7 @@
 package com.ticketon.ticketon.domain.waiting_queue.controller;
 
 import com.ticketon.ticketon.domain.waiting_queue.producer.QueueProducer;
+import com.ticketon.ticketon.exception.custom.UserMismatchException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,7 +9,6 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-@Slf4j
 @Controller
 public class QueueWebSocketController {
 
@@ -22,11 +22,15 @@ public class QueueWebSocketController {
 
     // 클라이언트가 보낸 userId로 순번 조회
     @MessageMapping("/queue-status")
-    @SendTo("/topic/position")
-    public Long getQueueStatus(String userId) {
-        return queueProducer.getMyQueuePosition(userId);
     @SendTo("/queue/position")
     public void getQueueStatus(String email, SimpMessageHeaderAccessor accessor) {
+
+        // 보안상 요청자와 세션이 동일한지 확인
+        String sessionUserEmail = accessor.getUser().getName();
+        if (!sessionUserEmail.equals(email)) {
+            throw new UserMismatchException(sessionUserEmail, email);
+        }
+
         Long position = queueProducer.getMyQueuePosition(email);
         messagingTemplate.convertAndSendToUser(email, "/queue/position", position);
     }
