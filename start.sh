50b0f7c43bc62d3c3529a81abf546fa82a9dfca2
onagu7167@gmail.com <72810708+yeongsol-Kim@users.noreply.github.com>
2025-06-29 18:14:14 +0900

    refactor: 대기열을 통과한 사용자만 티켓 구매에 접근할 수 있도록 수정

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@ echo "Building the jar with Gradle..."
 ./gradlew clean build -x test || { echo "Gradle build failed!"; exit 1; }
 
 echo "Building Docker images..."
-docker-compose -f docker-compose-local.yml build --no-cache || { echo "Docker build failed!"; exit 1; }
+docker compose -f docker-compose-local.yml build --no-cache || { echo "Docker build failed!"; exit 1; }
 
 echo "Starting containers..."
-docker-compose -f docker-compose-local.yml up -d || { echo "Docker compose up failed!"; exit 1; }
+docker compose -f docker-compose-local.yml up -d || { echo "Docker compose up failed!"; exit 1; }
 
 echo "All done! Containers are up and running."
