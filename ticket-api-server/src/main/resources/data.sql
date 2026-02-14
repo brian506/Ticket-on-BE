25229aae1425315cb9e84a6e024f3cdeffef2a59
brian <brian506@naver.com>
2026-02-15 08:40:57 +0900

    refactor : 재고 복구 로직에서 트랜잭션 범위 최소화

--- ticket-api-server/src/main/resources/data.sql
+++ ticket-api-server/src/main/resources/data.sql
@@ -1,13 +1,4 @@
-INSERT INTO members (email, password)
-WITH RECURSIVE seq AS (
-    SELECT 1 AS n
-    UNION ALL
-    SELECT n + 1 FROM seq WHERE n < 1000
-)
-SELECT
-    CONCAT('user', n, '@test.com'),
-    'pass1234'
-FROM seq;
+
 
 INSERT INTO event_items (title, start_date, end_date, event_status) VALUES
 ('2026 Winter Rock Festival', '2026-02-01', '2026-02-03', 'OPEN'),
